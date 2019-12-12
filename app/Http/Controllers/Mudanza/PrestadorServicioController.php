463aa39c8b8d4af2f4e9dfe77f88dd2fd745ae4f
Luis Julian <luisgerardojulianramirez2912@gmail.com>
2019-12-11 22:08:57 -0600

    busqueda prestador

--- app/Http/Controllers/Mudanza/PrestadorServicioController.php
+++ app/Http/Controllers/Mudanza/PrestadorServicioController.php
@@ -68,7 +68,8 @@ class PrestadorServicioController extends Controller
 	public function busquedaPrestadorServicio_id($id){
 		$prestador = PrestadorServicio::join('ranking','prestador_servicio.id_prestador','=','ranking.id_prestador')
 		->join('horario_tarifa','prestador_servicio.id_prestador','=','horario_tarifa.id_prestador')
-		->select('prestador_servicio.nombre','prestador_servicio.apellidos','prestador_servicio.direccion','prestador_servicio.telefono','prestador_servicio.correo','prestador_servicio.codigo_postal','prestador_servicio.foto_perfil','ranking.valoracion','horario_tarifa.precio','horario_tarifa.hora_inicio','horario_tarifa.hora_salida')
+		->join('vehiculos2','prestador_servicio.id_prestador','=','vehiculos2.id_prestador')
+		->select('prestador_servicio.nombre','prestador_servicio.apellidos','prestador_servicio.direccion','prestador_servicio.telefono','prestador_servicio.correo','prestador_servicio.codigo_postal','prestador_servicio.foto_perfil','ranking.valoracion','horario_tarifa.precio','horario_tarifa.hora_inicio','horario_tarifa.hora_salida','vehiculos2.foto_frontal','vehiculos2.foto_lateral','vehiculos2.foto_trasera','vehiculos2.capacidad_carga','vehiculos2.placas')
 		->where('prestador_servicio.id_prestador','=',$id)
 	   ->where('prestador_servicio.status', '=','1')
         ->get();
